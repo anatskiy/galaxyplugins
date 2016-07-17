c0d72fc43c7444e5728f2f6d6f769927b3c11dc2
anatskiy <evgeny.anatskiy@gmail.com>
2016-07-17 18:26:05 +0200

    improve rssreader app

--- rssreader/app.py
+++ rssreader/app.py
@@ -1,11 +1,33 @@
 from flask import Flask, render_template, request, jsonify
 
+import feedparser
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def index(name='index'):
-    return render_template('index.html', name=name)
+def index():
+    show_settings = False
+
+    # TODO: add permissions check (later)
+    if True:
+        show_settings = True
+
+    return render_template('index.html', show_settings=show_settings)
+
+
+@app.route('/get_rssfeed_news/', methods=['POST'])
+def get_feed_news():
+    url = request.form.get('feed_url')
+    feed = feedparser.parse(url)
+
+    news = [{
+                'title': entry['title'],
+                'url': entry['link'],
+                'description': entry['summary']
+            } for entry in feed['entries'][:10]]
+
+    return jsonify(news)
 
 
 if __name__ == '__main__':
--- rssreader/static/main.js
+++ rssreader/static/main.js
@@ -1,17 +1,136 @@
 $(document).ready(function () {
 
-    $('#rssfeeds').tabs();
+    // var RSSNews = Backbone.Model.extend({
+    //     defaults: {
+    //         title: '',
+    //         url: '',
+    //         description: ''
+    //     }
+    // });
+    //
+    // var RSSNewsList = Backbone.Collection.extend({
+    //     model: RSSNews,
+    //     url: '/get_rssfeed_news/'
+    // });
+
+    var RSSFeed = Backbone.Model.extend({
+        defaults: {
+            title: '',
+            url: '',
+            news: []
+            // news: new RSSNewsList
+        }
+    });
+
+    var RSSFeedList = Backbone.Collection.extend({
+        model: RSSFeed,
+        localStorage: new Backbone.LocalStorage('rssfeeds-backbone'),
+        comparator: 'order'
+    });
+
+    var RSSFeeds = new RSSFeedList;
+
+    var RSSFeedView = Backbone.View.extend({
+        tagName: 'li',
+
+        template: _.template($('#rssfeed-template').html()),
+        tabTemplate: _.template($('#rssfeedtab-template').html()),
+
+        events: {},
+
+        initialize: function () {
+            this.listenTo(this.model, 'destroy', this.remove);
+
+            // this.model.get('news').fetch(
+            //     {data: {feed_url: this.model.get('url')}, type: 'POST'}
+            // );
+        },
+
+        render: function () {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        },
+
+        renderTab: function (rssfeeds, index, numTotal) {
+            var me = this;
+            $.ajax({
+                url: "/get_rssfeed_news/",
+                type: "POST",
+                data: {feed_url: me.model.get('url')},
+                success: function (data) {
+                    me.model.set('news', data);
+                    rssfeeds.append(me.tabTemplate(me.model.toJSON()));
+
+                    // Only after all items have been added to the DOM,
+                    // initialize jQuery UI's tabs
+                    if (index == numTotal - 1) rssfeeds.tabs();
+                }
+            });
+        }
+    });
+
+    var RSSReaderAppView = Backbone.View.extend({
+        el: $('#rssreaderapp'),
+
+        emptyTemplate: _.template($('#rssfeedlist-empty-template').html()),
+
+        events: {
+            'click #showSettingsBtn': 'onShowSettingsBtnClick'
+        },
+
+        initialize: function () {
+            this.rssfeeds = $('#rssfeeds');
+            this.rssfeedList = $('#rssfeed-list');
+
+            this.settings = $('#rssreader-settings');
+            this.settings.dialog({
+                autoOpen: false,
+                resizable: false,
+                modal: true,
+                // draggable: false,
+                width: 400
+            });
+
+            RSSFeeds.fetch();
+            this.render();
+        },
+
+        render: function () {
+            if (RSSFeeds.length) {
+                // this.addAll();
+            } else {
+                this.rssfeeds.hide();
+                this.$el.append(this.emptyTemplate);
+            }
+        },
+
+        addOne: function (feed, index) {
+            var view = new RSSFeedView({model: feed});
+            this.rssfeedList.append(view.render().el);
+            view.renderTab(this.rssfeeds, index, RSSFeeds.length);
+        },
+
+        addAll: function () {
+            RSSFeeds.each(this.addOne, this);
+        },
+
+        onShowSettingsBtnClick: function () {
+            this.settings.dialog('open');
+        }
+    });
+
+    var RSSReaderApp = new RSSReaderAppView;
 
     // Show 'Delete Feed' button on Tab hover
-    $('#rssfeeds ul[role="tablist"] li').hover(
-        function () {
-            $(this).find('a').css('padding-right', 5);
-            $(this).find('span').css('display', 'block');
-        },
-        function () {
-            $(this).find('a').css('padding-right', 12);
-            $(this).find('span').css('display', 'none');
-        }
-    );
+    // $('#rssfeeds ul[role="tablist"] li').hover(
+    //     function () {
+    //         $(this).find('a').css('padding-right', 5);
+    //         $(this).find('span').css('display', 'block');
+    //     },
+    //     function () {
+    //         $(this).find('a').css('padding-right', 12);
+    //         $(this).find('span').css('display', 'none');
+    //     }
+    // );
 
 });
