fad513f2555f33a12cea6f3d6cd590f27339cfb0
Evgeny Anatskiy <evgeny.anatskiy@gmail.com>
2016-11-07 00:28:36 +0100

    improve price calculator app

--- price_calculator/static/script.js
+++ price_calculator/static/script.js
@@ -1,15 +1,47 @@
 $(document).ready(function() {
 
     // Third-party dependencies
-    // $('<script/>', {src: 'https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/9.0.0/nouislider.min.js'}).appendTo('head');
+    // $('<script/>', {src: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'}).appendTo('head');
     // $('<link/>', {
     //     rel: 'stylesheet',
     //     type: 'text/css',
     //     href: 'https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/9.0.0/nouislider.min.css'
     // }).appendTo('head');
 
+    var ControlCollection = Backbone.Collection.extend({});
+
+    var ProviderModel = Backbone.Model.extend({
+        initialize: function() {
+            this.set('_controls', new ControlCollection());
+        }
+    });
+
     var ProviderCollection = Backbone.Collection.extend({
-        url: 'api/webhooks/price_calculator/get_data'
+        url: 'api/webhooks/price_calculator/get_data',
+        model: ProviderModel
+    });
+
+    var ControlsView = Backbone.View.extend({
+        el: '#price_calculator-controls',
+
+        template: _.template([
+            '<% _controls.each(function(control) { %>',
+                '<div class="control-container">',
+                    '<div class="input-group">',
+                        '<input type="text" id="<%= control.attributes.name %>" class="form-control" placeholder="<%= control.attributes.label %>"></input>',
+                        '<span class="input-group-addon control-units"><%= control.attributes.units %></span>',
+                        '<% if (control.attributes.tooltip) { %>',
+                            '<span class="input-group-addon" data-toggle="tooltip" data-placement="right" title="<%= control.attributes.tooltip %>">?</span>',
+                        '<% } %>',
+                    '</div>',
+                '</div>',
+            '<% }); %>'
+        ].join('')),
+
+        render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        }
     });
 
     var PriceCalculatorAppView = Backbone.View.extend({
@@ -19,14 +51,31 @@ $(document).ready(function() {
             '<div id="price_calculator-header">',
                 '<h4>Price Calculator</h4>',
             '</div>',
-            '<div id="price_calculator-providers"></div>',
+            '<div id="price_calculator-providers">',
+                '<select id="provider-list"></select>',
+                '<hr/>',
+            '</div>',
             '<div id="price_calculator-controls"></div>',
             '<div id="price_calculator-total-price"></div>',
             '<div id="price_calculator-loader"></div>'
         ].join('')),
 
+        calculateButtonTemplate: _.template([
+            '<div class="text-centered">',
+                '<button id="calculateBtn" class="btn btn-default">Calculate</button>',
+            '</div>'
+        ].join('')),
+
+        totalPriceTemplate: _.template([
+            '<p>Total Price: ',
+                '<span class="total-price"><%= price %> </span>',
+                '<span class="total-price-currency"><%= currency %>/month</span>',
+            '</p>'
+        ].join('')),
+
         events: {
-            'change #provider-list': 'onProviderChange'
+            'change #provider-list': 'onProviderChange',
+            'click #calculateBtn': 'onCalculateBtnClick'
         },
 
         initialize: function() {
@@ -35,12 +84,10 @@ $(document).ready(function() {
             me.$el.html(this.appTemplate());
             me.providerCollection = new ProviderCollection();
             me.$calculatorProviders = $('#price_calculator-providers');
+            me.$providerList = $('#provider-list');
             me.$calculatorControls = $('#price_calculator-controls');
             me.$calculatorTotalPrice = $('#price_calculator-total-price');
             me.$loader = $('#price_calculator-loader');
-
-            // Listen to slider's "change" event
-            me.on('sliderchange', me.onSliderChange);
 
             // Hide Total Price
             me.$calculatorTotalPrice.hide();
@@ -48,6 +95,12 @@ $(document).ready(function() {
             // Get all information about providers
             me.providerCollection.fetch({
                 success: function(providers) {
+                    providers.each(function(provider) {
+                        provider.attributes._controls.add(provider.attributes.controls);
+                        provider.unset('controls');
+                    });
+
+                    me.$calculatorProviders.show();
                     me.$loader.remove();
                     me.render(providers);
                 }
@@ -57,16 +110,9 @@ $(document).ready(function() {
         render: function(providers) {
             var me = this;
 
-            // If Provider List is not added to DOM
-            if (!me.$providerList) {
-                me.$calculatorProviders.append($('<select/>', {id: 'provider-list'}));
-                me.$providerList = $('#provider-list');
-            }
-
             // Add providers to Provider List
             providers.each(function(provider) {
                 var model = provider.toJSON();
-
                 me.$providerList.append($('<option/>', {
                     text: model.name,
                     value: model.value
@@ -74,67 +120,48 @@ $(document).ready(function() {
             }, me);
 
             // Render all controls of a currently selected provider
-            me.renderControls(providers.models[0]);
+            var controlsView = new ControlsView({
+                model: providers.models[0]
+            });
+            controlsView.render();
 
-            // Render total price
-            me.renderTotalPrice(providers.models[0]);
+            // Initialize tooltips
+            $('[data-toggle="tooltip"]').tooltip();
+
+            // Show Calculate button
+            me.$calculatorControls.append(me.calculateButtonTemplate());
 
             return me;
         },
 
-        renderControls: function(provider) {
-            var me = this,
-                providerModel = provider.toJSON();
+        isValid: function(controls) {
+            var valid = false;
 
-            // Iterate over all provider's controls
-            _.each(providerModel.controls, function(control) {
-                var paramName = control.name.replace('_value', ''),
-                    $controlContainer = $('<div/>', {class: 'control-container'}),
-                    $controlLabel = $('<div/>', {class: 'control-label'})
-                        .append($('<span/>', {text: control.label}))
-                        .append($('<span/>', {
-                            class: 'label-desc',
-                            text:
-                                providerModel.params[paramName] +
-                                ' ' + providerModel.currency + ' per ' +
-                                control.min + ' ' + control.units
-                        })),
-                    $control = $('<div/>', {id: control.name});
+            controls.each(function(control) {
+                var model = control.toJSON(),
+                    $item = $('#' + model.name),
+                    value = $item.val();
 
-                $controlContainer
-                    .append($control)
-                    .append($controlLabel)
-                    .appendTo(me.$calculatorControls);
-
-                // Initialize noUiSlider's slider
-                noUiSlider.create($control[0], {
-                    connect: [true, false],
-                    tooltips: true,
-                    start: control.start,
-                    step: control.step,
-                    range: {
-                        min: control.min,
-                        max: control.max
-                    },
-                    pips: {
-                        mode: 'positions',
-                		values: [0, 100],
-                		density: 5
-                    },
-                    format: {
-                        to: function(value) {
-                            return parseInt(value);
-                        },
-                        from: function(value) {
-                            return value;
-                        }
-                    }
-                });
-
-                $control[0].noUiSlider.on('change', function() {
-                    me.trigger('sliderchange');
-                });
+                if (value !== '') {
+                    valid = true;
+                } else {
+                    // TODO: mark invalid (cannot be empty)
+                    valid = false;
+                }
             });
+
+            return valid;
+        },
+
+        onCalculateBtnClick: function() {
+            var provider = this.providerCollection.where({
+                value: this.$providerList.val()
+            })[0];
+            var controls = provider.attributes._controls;
+
+            if (this.isValid(controls)) {
+                this.renderTotalPrice(provider);
+            }
         },
 
         renderTotalPrice: function(provider) {
@@ -147,23 +174,18 @@ $(document).ready(function() {
                 formula = formula.replace(name, value);
             });
 
-            // // Replace all slider values in the formula
-            _.each(providerModel.controls, function(control) {
-                var name = control.name,
-                    value = $('#' + name)[0].noUiSlider.get();
+            // // Replace all input values in the formula
+            providerModel._controls.each(function(control) {
+                var name = control.attributes.name,
+                    value = parseFloat($('#' + name).val());
                 formula = formula.replace(name, value);
             });
 
-            var totalPrice = eval(formula); // jshint ignore:line
-
-            me.$calculatorTotalPrice.empty();
-            // me.$calculatorTotalPrice.append($('<p/>', {
-            me.$calculatorTotalPrice.append($(
-                '<p>Total Price: ' +
-                    '<span class="total-price">' + totalPrice.toFixed(2) + '</span> ' +
-                    '<span class="total-price-currency">' + providerModel.currency + '</span>' +
-                '</p>'
-            ));
+            var totalPrice = eval(formula);  // jshint ignore:line
+            me.$calculatorTotalPrice.html(me.totalPriceTemplate({
+                price: totalPrice.toFixed(2),
+                currency: providerModel.currency
+            }));
 
             // Show Total Price
             me.$calculatorTotalPrice.show();
@@ -177,16 +199,9 @@ $(document).ready(function() {
             this.$calculatorTotalPrice.empty();
             this.renderControls(provider);
             this.renderTotalPrice(provider);
-        },
-
-        onSliderChange: function() {
-            var providerValue = this.$providerList.val(),
-                provider = this.providerCollection.where({value: providerValue})[0];
-
-            this.renderTotalPrice(provider);
         }
     });
 
-    var PriceCalculatorApp = new PriceCalculatorAppView();
+    new PriceCalculatorAppView();
 
 });
